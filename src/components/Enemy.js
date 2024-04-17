512aa8f3eb35226518edf3c66c4e7fff3fe00810
Ujjawal Verma <ujjawal.verma9651@gmail.com>
2024-04-17 13:41:15 +0530

    v1 done

--- src/components/Enemy.js
+++ src/components/Enemy.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState} from 'react'
-
+import { IoReloadOutline } from "react-icons/io5";
 import {generate} from "random-words";
+import { FaKeyboard } from "react-icons/fa";
 
 const Enemy = () => {
 
@@ -16,6 +17,8 @@ const [gameStart,setGameStart]=useState(true);
 const [gameEnd,setGameEnd]=useState(false);
 let time=30;
 const [timer,setTimer]=useState(time);
+const [wrong,setWrong]= useState(0);
+
 
 let watch=()=>{
   setInterval(()=>{
@@ -40,7 +43,7 @@ let watch=()=>{
     if(gameStart){
    
     if(e.key===' ' && innerIndex===word[currentIndex].length-1 && wrongIndex===-1){
-      
+    
       setIndex((currentIndex)=> currentIndex+1);
       setWordCount((wordCount)=>wordCount+1);
       if(currentIndex===word.length-1){
@@ -76,6 +79,7 @@ let watch=()=>{
       if(wrongIndex>innerIndex+1 || wrongIndex===-1){
       setWrongIndex(innerIndex+1);}
       setInnerIndex((innerIndex)=>innerIndex+1);
+      setWrong((wrong)=>wrong+1);
       
     }
     if(e.key===word[currentIndex][innerIndex+1]){
@@ -104,11 +108,12 @@ let watch=()=>{
 
     
   return (
-    <div>  
+    <div> 
+      <div className='text-2xl md:text-5xl w-fit font-mono  p-5 mt-3 underline md:mb-16 md:mx-16 mb-10 font-bold text-yellow-500 items-center flex gap-3'>TYPE WARRIOR <FaKeyboard size={40} color='white'/></div> 
         
-        <div className={(gameEnd&&!gameStart)?"hidden":" text-2xl font-bold text-yellow-600 mx-20 px-5 mt-10 flex font-serif justify-between"} ><div><span className='text-white'>Timer - </span>{timer}</div><div><span className='text-white'> Word Count - </span>{wordCount}</div></div>
-        <div className={(gameEnd&&!gameStart)?"hidden":'main text-gray-500 flex items-center mx-20 p-5   mt-9 tracking-wider '}>
-        <div className='text-3xl '>
+        <div className={(gameEnd&&!gameStart)?"hidden":" md:text-2xl font-bold text-yellow-600 md:mx-16 px-5 mt-10 flex font-serif justify-between"} ><div><span className='text-white'>Timer - </span>{timer} s</div><div><span className='text-white'> Word Count - </span>{wordCount}</div></div>
+        <div className={(gameEnd&&!gameStart)?"hidden":'main text-gray-500 flex items-center md:mx-16 p-5   mt-9 tracking-wider '}>
+        <div className='text-xl md:text-3xl '>
       {word.map((element,index)=>{
         if(index===currentIndex){
 
@@ -128,12 +133,15 @@ let watch=()=>{
     </div>
       
    
-      <div className={(gameEnd&&!gameStart)?"w-fit mx-auto mt-12":"hidden"}>
-        <div className='text-3xl font-serif font-bold cursor-default '><span className='text-white '>AVG SPEED - </span> <span className='text-red-500'>{wordCount*2} wpm</span>   </div>
-        <></>
+      <div className={(gameEnd&&!gameStart)?"w-fit mx-auto mt-12 space-y-9":"hidden"}>
+        <div className='text-xl md:text-3xl font-serif font-bold cursor-default '><span className='text-white '>AVG SPEED - </span> <span className='text-red-500'>{wordCount*2} wpm</span>   </div>
+        <div className='text-xl md:text-3xl font-serif font-bold cursor-default '><span className='text-white '>Wrong Chars - </span> <span className='text-red-500'>{wrong} </span>   </div>
+        <br />
+        <br />
         
 
       </div>
+      <div className='w-fit mx-auto mt-7 cursor-pointer hover:scale-125 transition-all' onClick={()=>{ window.location.reload(); }}> <IoReloadOutline size={30} color='white'/></div>
 
     
     </div>
